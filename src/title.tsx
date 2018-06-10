b41cb73a2d454194dd4006ef9132e7994aeb2f82
Ashley Davis <ashley@codecapers.com.au>
2018-06-10 20:01:09 +1000

    Removed the depdendency on Inversify. Don't appear to need this.

--- src/title.tsx
+++ src/title.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { IEnv } from './env';
-import {Inject} from 'react.di';
+import { Inject } from 'react.di';
 
 export class Title extends React.Component {
 
